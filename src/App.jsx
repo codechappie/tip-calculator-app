ad2c54149450d8942725984b7c639e39aae51ed9
codechappie <davidproyecto01@gmail.com>
2021-08-08 23:29:49 -0500

    upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import TipCalculator from "./modules/tip-calculator/TipCalculator";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="title">
+        <div>SPLI</div>
+        <div>TTER</div>
+      </div>
+      <TipCalculator />
+      <div className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" rel="noreferrer" target="_blank">Frontend Mentor</a>.
+        Coded by <a href="https://www.codechappie.com" target="_blank" rel="noreferrer">CodeChappie</a>.
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/modules/tip-calculator/TipCalculator.jsx
@@ -0,0 +1,124 @@
+import style from './tip-calculator.module.scss'
+import dollarIcon from '../../assets/images/icon-dollar.svg'
+import personIcon from '../../assets/images/icon-person.svg'
+import { useEffect, useState } from 'react'
+const TipCalculator = () => {
+    const [bill, setBill] = useState(0);
+    const [numberOfPeople, setNumberOfPeople] = useState(0);
+    const [percentage, setPercentage] = useState(0);
+    const [tip, setTip] = useState(0);
+    const [total, setTotal] = useState(0);
+    const [resetButton, setResetButton] = useState(true)
+
+
+    const resetInputs = () => {
+        setPercentage(0)
+        setNumberOfPeople(0)
+        setBill(0)
+    }
+
+    useEffect(() => {
+        console.log("change...")
+        console.log(parseFloat(percentage))
+        console.log(parseInt(numberOfPeople))
+        console.log(parseFloat(bill))
+        if (percentage > 0 && bill > 0 && numberOfPeople > 0) {
+            let actualTip = (bill / numberOfPeople) * (percentage / 100);
+            setTip(actualTip)
+            setTotal((bill / numberOfPeople) + actualTip)
+            setResetButton(false)
+        } else {
+            setResetButton(true)
+        }
+    }, [percentage, bill, numberOfPeople, resetButton])
+
+
+
+    const changeSelectedBtn = (e, perc) => {
+        const buttons = document.querySelectorAll(".btn")
+        const activeBtn = document.getElementById(`${e.target.id}`);
+        // console.log(activeBtn)
+        // console.log(buttons)
+        buttons.forEach((btn) => {
+            btn.classList.remove("active");
+        })
+        activeBtn.classList.add("active")
+        setPercentage(perc)
+    }
+
+    return (
+        <div className={style.tip__calculator}>
+            <form className={style.tip__form}>
+                <label htmlFor="">Bill</label>
+                <div className={style.input}>
+                    <img src={dollarIcon} alt="" />
+                    <input type="number" autoFocus={true} placeholder="0" value={bill} onChange={e => setBill(e.target.value)} />
+                </div>
+
+
+                <label className={style.select__tip__label} htmlFor="">Select Tip %</label>
+                <div className={style.select__tip}>
+                    <div className={`${style.button} btn`} id="btn1" onClick={(e) => changeSelectedBtn(e, 5)}>
+                        5%
+                    </div>
+                    <div className={`${style.button} btn`} id="btn2" onClick={(e) => changeSelectedBtn(e, 10)}>
+                        10%
+                    </div>
+                    <div className={`${style.button} btn`} id="btn3" onClick={(e) => changeSelectedBtn(e, 15)}>
+                        15%
+                    </div>
+                    <div className={`${style.button} btn`} id="btn4" onClick={(e) => changeSelectedBtn(e, 25)}>
+                        25%
+                    </div>
+                    <div className={`${style.button} btn`} id="btn5" onClick={(e) => changeSelectedBtn(e, 50)}>
+                        50%
+                    </div>
+
+                    <input type="number" value={percentage} onChange={e => setPercentage(e.target.value)} placeholder="Custom" className={`button btn ${style.percentage}`} id="btn6" onClick={(e) => changeSelectedBtn(e, 0)} />
+
+                </div>
+
+                <label htmlFor="">Number of People</label>
+                <div className={style.input}>
+                    <img src={personIcon} alt="" />
+                    <input type="number" placeholder="0" value={numberOfPeople} onChange={e => setNumberOfPeople(e.target.value)} />
+                </div>
+            </form>
+
+            <div className={style.result}>
+                <div>
+                    <div className={style.row}>
+                        <div>
+                            <h5>Tip Amount</h5>
+                            <h6>/ person</h6>
+                        </div>
+
+                        <div className={style.price}>
+                            ${tip.toFixed(2)}
+                        </div>
+                    </div>
+
+                    <div className={style.row}>
+                        <div>
+                            <h5>Total</h5>
+                            <h6>/ person</h6>
+                        </div>
+
+                        <div className={style.price}>
+                            ${total.toFixed(2)}
+                        </div>
+                    </div>
+                </div>
+
+                <button
+                    className={`${style.button} ${style.reset__button} ${resetButton ? `${style.disabled}` : ''}`}
+                    onClick={resetInputs}>
+                    Reset
+                </button>
+            </div>
+
+        </div >
+    )
+}
+
+export default TipCalculator
